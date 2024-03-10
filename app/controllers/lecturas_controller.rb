078a3be3c0385495e452cf3ae86c0c9b356f776e
Mauri <mauricio_bordon@outlook.com>
2024-03-10 13:28:50 -0300

    implementacion de borradores de libros con capitulos no pubicados

--- app/controllers/lecturas_controller.rb
+++ app/controllers/lecturas_controller.rb
@@ -63,14 +63,13 @@ class LecturasController < ApplicationController
   def capitulo_actual
     user = get_user
     if user.nil?
-      render json: {error: "El usuario no se encuentra"}, status: 400
-      return
+      return render json: {error: "El usuario no se encuentra"}, status: 400
     end
-
     @lectura = Lectura.find_by(user_id: user.id, libro_id: params[:libro_id])
     capitulo_actual =  @lectura.capitulo_id
     capitulo = Capitulo.find_by(id: capitulo_actual)
-    render json: { capitulo_actual: CapituloSerializer.new(capitulo)}
+    capitulo.contenido = obtener_contenido(capitulo.nombre_archivo)
+    return render json: { capitulo_actual: CapituloSerializer.new(capitulo)}, status: 200
   end
 
   # GET /libros_en_progreso
@@ -86,10 +85,35 @@ class LecturasController < ApplicationController
                                .order(updated_at: :desc)
                                .distinct
                                .paginate(page: params[:page])
+    @libros_en_progreso.each do |libro|
+      libro.portada = obtener_portada(libro.portada)
+    end
 
     render json: @libros_en_progreso, status: 200
   end
 
+  def libros_con_capitulos_no_publicados
+    user = get_user
+    if user.nil?
+      render json: {error: "El usuario no se encuentra"}, status: 400
+      return
+    end
+    # Encuentra todos los libros del usuario actual que tienen al menos un capítulo no publicado
+    libros = user.libros.includes(:capitulos).where(capitulos: { publicado: false }).distinct
+
+    # Itera sobre cada libro para encontrar el último capítulo no publicado actualizado más recientemente
+    libros_con_ultimo_capitulo_no_publicado = libros.map do |libro|
+      ultimo_capitulo_no_publicado = libro.capitulos.where(publicado: false).order(updated_at: :desc).first
+      ultimo_capitulo_no_publicado.contenido = obtener_contenido(ultimo_capitulo_no_publicado.nombre_archivo)
+      capitulo = CapituloForOwnerSerializer.new(ultimo_capitulo_no_publicado)
+
+      libro.portada = obtener_portada(libro.portada)
+      libro = LibroSerializer.new(libro)
+      { libro: libro, ultimo_capitulo_no_publicado: capitulo }
+    end
+
+    render json: libros_con_ultimo_capitulo_no_publicado, status: 200
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -97,6 +121,32 @@ class LecturasController < ApplicationController
       @lectura = Lectura.find(params[:id])
     end
 
+  def obtener_contenido(contenido_public_id)
+    if !contenido_public_id
+      return ""
+    end
+
+    begin
+      enlace_temporal = Cloudinary::Utils.cloudinary_url("#{contenido_public_id}", :resource_type => :raw, :expires_at => (Time.now + 3600).to_i)
+      return enlace_temporal
+    rescue CloudinaryException => e
+      return ""
+    end
+  end
+
+  def obtener_portada(portada_public_id)
+    if !portada_public_id
+      return ""
+    end
+
+    begin
+      enlace_temporal = Cloudinary::Utils.cloudinary_url("#{portada_public_id}", :resource_type => :image, :expires_at => (Time.now + 3600).to_i)
+      return enlace_temporal
+    rescue CloudinaryException => e
+      puts "Error al obtener la portada de Cloudinary: #{e.message}"
+      return ""
+    end
+  end
     # Only allow a list of trusted parameters through.
     def lectura_params
       params.require(:lectura).permit(:user_id, :libro_id, :capitulo_id, :terminado)
