6c7bb4629d2a6fdc16c3fd7c24d6c638eeaf586d
Mauri <mauricio_bordon@outlook.com>
2024-03-29 10:21:36 -0400

    cambios a lecturas

--- app/models/lectura.rb
+++ app/models/lectura.rb
@@ -9,7 +9,7 @@ class Lectura < ApplicationRecord
 
   attribute :deleted, :boolean, default: false
   attribute :terminado, :boolean, default: false
-
+  attribute :terminado, :leido, default: false
   has_many :fecha_lecturas
   def self.create_lectura(params, user)
     libro_id = params[:libro_id]
@@ -29,13 +29,25 @@ class Lectura < ApplicationRecord
     leyendo = Lectura.find_by(libro_id: libro_id, user_id: user.id)
 
     if leyendo.present?
-      leyendo.update(capitulo_id: capitulo_id, terminado: params[:terminado], leido:params[:leido] , deleted: false)
+      leido = params[:leido]
+      if leido.nil?
+        leyendo.update(capitulo_id: capitulo_id, terminado: params[:terminado] , deleted: false)
+      else
+        leyendo.update(capitulo_id: capitulo_id, terminado: params[:terminado], leido:params[:leido] , deleted: false)
+      end
+
       #Guardar la fecha de lectura
       fecha_lectura = FechaLectura.new(lectura_id: leyendo.id, user_id: user.id, libro_id: libro_id, fecha: Time.now)
       fecha_lectura.save
       return { message: 'Progreso de lectura actualizado exitosamente', lectura: LecturaSerializer.new(leyendo) }, :created
     else
-      lectura = Lectura.new(user_id: user.id, libro_id: libro_id, capitulo_id: capitulo_id, terminado: params[:terminado] , leido:params[:leido] , deleted: false)
+      leido = params[:leido]
+      if leido.nil?
+        lectura = Lectura.new(user_id: user.id, libro_id: libro_id, capitulo_id: capitulo_id, terminado: params[:terminado] , deleted: false)
+      else
+        lectura = Lectura.new(user_id: user.id, libro_id: libro_id, capitulo_id: capitulo_id, terminado: params[:terminado] , leido:params[:leido] , deleted: false)
+      end
+
       #Guardar la fecha de lectura
       fecha_lectura = FechaLectura.new(lectura_id: lectura.id, user_id: user.id, libro_id: libro_id, fecha: Time.now)
       fecha_lectura.save
