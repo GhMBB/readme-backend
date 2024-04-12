963fd0e6dbe2ae973b7a02ab5b1eeea221d07346
Mauri <mauricio_bordon@outlook.com>
2024-04-12 18:44:25 -0400

    implementacion de endpoint para relacion entre usuarios y seguidores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_request
   before_action :set_user, only: %i[show]
 
-  #rescue_from StandardError, with: :internal_server_error
+  rescue_from StandardError, with: :internal_server_error
 
   # GET /users/1
   def show
