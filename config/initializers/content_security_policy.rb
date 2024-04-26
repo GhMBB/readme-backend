73345da5e43cb6528e256bb0490a870f4931658b
Mauri <mauricio_bordon@outlook.com>
2024-04-26 11:37:16 -0400

    content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,6 +1,7 @@
+=begin
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self
   policy.script_src :self, :https
   policy.style_src :self, :https
   policy.img_src :self, :https, :data
-end
+end=end
