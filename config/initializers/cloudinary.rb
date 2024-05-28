4785396f6627a0ac64facbf7462f219847313b94
= <=>
2024-05-28 10:28:10 -0400

    Test de integracion y servidor de cloudinary de test

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,11 @@
 Cloudinary.config do |config|
-    config.cloud_name = 'dkrmah0f7'
-    config.api_key = '183549582925518'
-    config.api_secret = 'LRDlCyJxSBI03yiQzOGQZpBdrvA'
-end
+    if Rails.env.test?
+      config.cloud_name = 'docca4t8j'
+      config.api_key = '466253913145971'
+      config.api_secret = 'ajZCzPjbtjQFrZTj0J2Jj0yT7eA'
+    else
+      config.cloud_name = 'dkrmah0f7'
+      config.api_key = '183549582925518'
+      config.api_secret = 'LRDlCyJxSBI03yiQzOGQZpBdrvA'
+    end
+  end
